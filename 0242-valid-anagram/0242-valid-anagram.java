4a65d3b2804e120fc1a892dceb1209372151051c
Suhaas Bandari <123404240+suhaascodes@users.noreply.github.com>
2026-06-10 02:53:28 +0530

    Time: 95 ms (5.66%), Space: 44.1 MB (89.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
+        if (s.length() != t.length()) return false;
         int[] freq = new int[26];
-        int n = s.length();
-        for(int i=0; i<n; i++) {
-            char c1 = s.charAt(i);
-            int ind1 = c1 - 'a';
+        for (int i=0; i<s.length(); i++) {
+            char ch1 = s.charAt(i);
+            int ind1 = ch1 - 'a';
             freq[ind1]++;
-            char c2 = t.charAt(i);
-            int ind2 = c2 - 'a';
+            char ch2 = t.charAt(i);
+            int ind2 = ch2 - 'a';
             freq[ind2]--;
         }
-        for (int num: freq) {
+        for (int num:freq) {
             if (num != 0) return false;
         }
         return true;
