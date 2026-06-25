badcedb162e92e693b8428cb0304f6a4caeb0085
Suhaas Bandari <123404240+suhaascodes@users.noreply.github.com>
2026-06-25 13:19:18 +0530

    Time: 15 ms (94.11%), Space: 45.8 MB (62.35%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,19 +1,18 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
+        int n  = nums.length;
         Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
         for (int i=0; i<n-3; i++) {
-            if (i>0 && nums[i] == nums[i-1]) continue;
-            // long target1 = target - nums[i];
+            if (i > 0 && nums[i] == nums[i-1]) continue;
             for (int j=i+1; j<n-2; j++) {
                 if (j>i+1 && nums[j] == nums[j-1]) continue;
-                long target2 = (long) target - nums[i] - nums[j];
+                long target1 = (long) target - nums[i] - nums[j];
                 int left = j+1;
                 int right = n-1;
                 while (left < right) {
                     long sum = (long) nums[left] + nums[right];
-                    if (sum == target2) {
+                    if (sum == target1) {
                         res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
                         while (left < right && nums[left] == nums[left+1]) {
                             left++;
@@ -23,7 +22,7 @@ class Solution {
                         }
                         left++;
                         right--;
-                    } else if (sum < target2) {
+                    } else if (sum < target1) {
                         left++;
                     } else {
                         right--;
