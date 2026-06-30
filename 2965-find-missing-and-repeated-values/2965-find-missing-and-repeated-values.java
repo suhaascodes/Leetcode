ead99980afa31262ab2f2102bcbec130be6255e6
Suhaas Bandari <123404240+suhaascodes@users.noreply.github.com>
2026-07-01 00:49:10 +0530

    Time: 1 ms (99.98%), Space: 47.6 MB (10.28%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -2,17 +2,17 @@ class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int n = grid.length;
         int[] ans = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] freq = new int[n*n+1];
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                map.put(grid[i][j], map.getOrDefault(grid[i][j], 0) + 1);
+                freq[grid[i][j]]++;
             }
         }
         for (int i=1; i<=n*n; i++) {
-            if (map.get(i) == null) {
-                ans[1] = i;
-            }else if (map.get(i) == 2) {
+            if (freq[i] == 2) {
                 ans[0] = i;
+            } else if (freq[i] == 0) {
+                ans[1] = i;
             }
         }
         return ans;
