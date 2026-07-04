210034895de147590430e5a242668eff259f9ffd
Suhaas Bandari <123404240+suhaascodes@users.noreply.github.com>
2026-07-04 20:47:25 +0530

    Time: 16 ms (94.21%), Space: 45.7 MB (76.92%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n  = nums.length;
+        int n = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         for (int i=0; i<n-3; i++) {
             if (i > 0 && nums[i] == nums[i-1]) continue;
             for (int j=i+1; j<n-2; j++) {
-                if (j>i+1 && nums[j] == nums[j-1]) continue;
-                long target1 = (long) target - nums[i] - nums[j];
+                if (j > i+1 && nums[j] == nums[j-1]) continue;
                 int left = j+1;
                 int right = n-1;
+                long tar = (long)target - nums[i] - nums[j];
                 while (left < right) {
-                    long sum = (long) nums[left] + nums[right];
-                    if (sum == target1) {
+                    long sum = (long)nums[left] + nums[right];
+                    if (sum == tar) {
                         res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
                         while (left < right && nums[left] == nums[left+1]) {
                             left++;
@@ -22,7 +22,7 @@ class Solution {
                         }
                         left++;
                         right--;
-                    } else if (sum < target1) {
+                    } else if (sum < tar) {
                         left++;
                     } else {
                         right--;
