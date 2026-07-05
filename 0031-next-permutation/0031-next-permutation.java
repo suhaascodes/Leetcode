725bc55226953233122b9dce738282ca92e25e6b
Suhaas Bandari <123404240+suhaascodes@users.noreply.github.com>
2026-07-05 15:03:29 +0530

    Time: 0 ms (100%), Space: 44.7 MB (32.02%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,17 +1,29 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        boolean swapped = false;
+        int left = n-2;
+        int right = n-1;
         int pivot = 0;
-        for (int i=n-1; i>0; i--) {
-            if (nums[i] > nums[i-1]) {
-                pivot = i-1;
+        boolean swapped = false;
+        while (left >= 0 && right > 0) {
+            if (nums[right] > nums[left]) {
+                pivot = left;
                 swapped = true;
                 break;
             }
+            left--;
+            right--;
         }
-        if (!swapped) {
-            int low = 0;
+        if (swapped) {
+            for (int i=n-1; i>pivot; i--) {
+                if (nums[i] > nums[pivot]) {
+                    int temp = nums[pivot];
+                    nums[pivot] = nums[i];
+                    nums[i] = temp;
+                    break;
+                }
+            }
+            int low = pivot+1;
             int high = n-1;
             while (low < high) {
                 int temp = nums[low];
@@ -21,15 +33,7 @@ class Solution {
                 high--;
             }
         } else {
-            for (int i=n-1; i>0; i--) {
-                if (nums[i] > nums[pivot]) {
-                    int temp = nums[i];
-                    nums[i] = nums[pivot];
-                    nums[pivot] = temp;
-                    break;
-                }
-            }
-            int low = pivot+1;
+            int low = 0;
             int high = n-1;
             while (low < high) {
                 int temp = nums[low];
