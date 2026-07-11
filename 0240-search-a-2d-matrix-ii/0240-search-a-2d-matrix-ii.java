e04fb8b36bc6fc3a2cd6f7fcfdb68241eebd839b
Suhaas Bandari <123404240+suhaascodes@users.noreply.github.com>
2026-07-12 00:10:42 +0530

    Time: 3 ms (89.59%), Space: 48.3 MB (25.88%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -4,7 +4,7 @@ class Solution {
         int n = matrix[0].length;
         int i = 0;
         int j = n-1;
-        while (i < m && i >= 0 && j < n && j >= 0) {
+        while (i < m && j >= 0) {
             int num = matrix[i][j];
             if (num == target) {
                 return true;
